4134450944636c32093fa56b000ac14e69c3bf8f
Thongchai Subsaidee <chanoktrue@me.com>
2021-11-02 05:11:27 +0700

    Initial Commit

---
+++ TodoListCoreDataDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoListCoreDataDemo
+//
+//  Created by Thongchai Subsaidee on 2/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListCoreDataDemo/TodoListCoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListCoreDataDemoApp.swift
+//  TodoListCoreDataDemo
+//
+//  Created by Thongchai Subsaidee on 2/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoListCoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
